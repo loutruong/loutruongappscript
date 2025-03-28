5749b8e64c97873b133eec50cc282b684c5f7473
lou_truong <thanhvantruong.vn@gmail.com>
2025-03-28 19:31:04 +0700

    Add time stamp function

--- code.js
+++ code.js
@@ -1,31 +1,67 @@
-// Global variable declaration
-const spreadSheetID = "1BYDLENNlvp6yarpq0UUPPwHYj15yRSK-qBOaS80Vs1w"; // Controller
-const sheetName_1 = "Inc lv1 job tracking"; // Controller
-const sheetId_1 = "281717863"; // Controller
-
+// Global variable declaration and store information to reuse
+// const spreadSheetID = "1BYDLENNlvp6yarpq0UUPPwHYj15yRSK-qBOaS80Vs1w"; // Controller
+// const sheetName_1 = "Inc lv1 job tracking"; // Controller
+// const sheetId_1 = "281717863"; // Controller
+// const ss_1 = SpreadsheetApp.openById(spreadSheetID).getSheetByName(sheetName_1); // Fixed
 // Function to get sheet id
-function checkSheetId() {
-  let ss = SpreadsheetApp.openById(spreadSheetID).getSheetByName(sheetName_1);
-  try {
-    if (ss) {
-      console.log(`The actual ID for ${sheetName_1} is: ${ss.getSheetId()}`);
-    } else {
-      console.log(`The actual ID for ${sheetName_1} is not found`);
-    }
-  } catch (error) {
-    console.error("Error message:", error.message);
-  }
-}
+// function checkSheetId() {
+//   try {
+//     if (ss_1) {
+//       console.log(`The actual ID for ${sheetName_1} is: ${ss_1.getSheetId()}`);
+//     } else {
+//       console.log(`The actual ID for ${sheetName_1} is not found`);
+//     }
+//   } catch (error) {
+//     console.error("Error message:", error.message);
+//   }
+// }
 
 // Function callback onEdit
 function onEdit(e) {
-  timeStamp(e);
+  try {
+    if (e) {
+      addTimeStamp(e);
+    } else {
+      console.error("onEdit triggered without event object 'e'.");
+    }
+  } catch (error) {
+    console.error("Error in onEdit:", error.message);
+  }
 }
 
-// Function handle timeStamp feature
-
-function timeStamp(e) {
-  if (e) {
-    e.source.getAcitveSheet().getRange("A11:A12").setValue("Testing");
+function addTimeStamp(e) {
+  const sheetId_1 = 281717863; // Controller
+  try {
+    if (
+      e &&
+      e.range.getRow() > 1 &&
+      e.range.getColumn() >=
+        SpreadsheetApp.getActiveSpreadsheet()
+          .getActiveSheet()
+          .getRange("C1")
+          .getColumn() &&
+      e.range.getColumn() <=
+        SpreadsheetApp.getActiveSpreadsheet()
+          .getActiveSheet()
+          .getRange("H1")
+          .getColumn() &&
+      e.source.getSheetId() === sheetId_1 &&
+      e.source.getActiveSheet().getRange(e.range.getRow(), 1).getValue() === ""
+    ) {
+      e.source
+        .getActiveSheet()
+        .getRange(e.range.getRow(), 1)
+        .setValue(
+          Utilities.formatDate(
+            new Date(),
+            "Asia/Ho_Chi_Minh",
+            "yyyy-MM-dd HH:mm:ss"
+          )
+        );
+    } else {
+      console.error("addTimeStamp error");
+    }
+  } catch (error) {
+    console.error("Error message:", error.message);
   }
 }
