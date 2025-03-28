a0dd25b20918e977c5fddaaece4e6ebbfcb3428d
lou_truong <thanhvantruong.vn@gmail.com>
2025-03-28 23:41:05 +0700

    Add generate ID link feature

--- 01. Lou Truong BRD - Automation by onEdit.js
+++ 01. Lou Truong BRD - Automation by onEdit.js
@@ -36,13 +36,36 @@ function onEdit(e) {
 // Function add id
 function linkBuilderIdGenerate(e) {
   const sheetId_1 = 462100576; // Controller
-  if (
-    e &&
-    e.range.getRow() > 1 &&
-    e.range.getColumn() === 2 &&
-    e.source.getSheetId() === sheetId_1 &&
-    e.source.getActiveSheet().getRange(e.range.getRow(), 1).getValue() === ""
-  ) {
-    e.source.getActiveSheet().getRange(e.range.getRow(), 1).setValue("Test");
+  const idPrefix = "Link_"; // The prefix for the ID
+  try {
+    if (
+      e &&
+      e.range.getRow() > 1 &&
+      e.range.getColumn() === 2 &&
+      e.source.getSheetId() === sheetId_1 &&
+      e.source.getActiveSheet().getRange(e.range.getRow(), 1).getValue() === ""
+    ) {
+      let maxIdNumber = 0;
+      const idValues = e.range
+        .getSheet()
+        .getRange(2, 1, e.range.getSheet().getMaxRows() - 1)
+        .getValues();
+      for (let i = 0; i < idValues.length; i++) {
+        const value = idValues[i][0]; // Get the value from the cell [row][col=0]
+        if (typeof value === "string" && value.startsWith(idPrefix)) {
+          const curIdNumber = parseInt(value.substring(idPrefix.length), 10);
+          if (!isNaN(curIdNumber)) {
+            maxIdNumber = Math.max(maxIdNumber, curIdNumber); // Update the maximum number found
+          }
+        }
+      }
+      const nextIdNumber = maxIdNumber + 1;
+      const newId = idPrefix + nextIdNumber; // e.g., "Link_1", "Link_2", etc.
+      e.range.getSheet().getRange(e.range.getRow(), 1).setValue(newId);
+    }
+  } catch (error) {
+    console.error(
+      `Error in onEdit handler: ${error.message}\nStack: ${error.stack}`
+    );
   }
 }
